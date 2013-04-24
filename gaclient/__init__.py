0f502a15011b0350221c3c6b5425e075269f8751
Tiemo Kieft <t.kieft@social-mining.com>
2013-04-24 10:02:18 +0200

    created setup.py

--- gaclient/__init__.py
+++ gaclient/__init__.py
@@ -2,14 +2,7 @@
     A client for google analytics.
 '''
 
-__author__ = 'Tiemo Kieft <t.kieft@gmail.com>'
-__copyright__ = 'Copyright 2013, Tiemo Kieft'
-__credits__ = ['Tiemo Kieft']
-__license__ = 'Apache'
 __version__ = '0.0.1'
-__maintainer__ = 'T. Kieft'
-__email__ = 't.kieft@gmail.com'
-__status__ = 'Development'
 
 import logging
 
---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from distutils.core import setup
+
+import gaclient
+
+setup(name='gaclient',
+    version=gaclient.__version__,
+    description='A easy to use interface to Google Analytics.',
+    long_description='''This package provides an easy to use interface to Google Analytics.''',
+    author='Tiemo Kieft',
+    author_email='t.kieft@gmail.com',
+    url='https://github.com/blubber/gaclient',
+    license="Apache 2.0",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    platform=('Any',),
+    keywords = ('google analytics'),
+    packages=['gaclient'],
+    requires=['apiclient (>=1.0)'])
